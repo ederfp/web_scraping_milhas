5545d954540e025be34fc3f247d31b55d3b6144e
Eder Fornielles Pereira <ederfpereira@gmail.com>
2024-06-04 01:28:14 -0300

    update

--- web_sraping_milhas.py
+++ web_sraping_milhas.py
@@ -2,27 +2,70 @@ from bs4 import BeautifulSoup
 import requests
 
 
-smiles_url = 'https://www.smiles.com.br/home'
+class Milhas:
 
-page = requests.get(smiles_url, verify=False)
-resposta = page.text
-soup = BeautifulSoup(resposta, 'html.parser')
+    def __init__(self) -> None:
+        pass
 
-title1 = soup.find_all('span', {'id': 'part1'})
-title2 = soup.find_all('span', {'id': 'part2'})
-desc = soup.find_all('p', {'id': 'banner_description'})
-link = soup.find_all('a', {'class': 'liferay-home-banner-action'})
+    def smiles(self):
+        smiles_url = 'https://www.smiles.com.br/home'
 
-title1_list = [i1.text for i1 in title1]
-title2_list = [i2.text for i2 in title2]
-desc_list = [i3.text.strip() for i3 in desc]
-link_list = [i4['href'] for i4 in link]
+        page = requests.get(smiles_url, verify=False)
+        resposta = page.text
+        soup = BeautifulSoup(resposta, 'html.parser')
 
-print('Promoções Smiles \n')
-for i, promo in enumerate(title1_list):
-    # print(title1_list)
-    try:
-        print(
-            f'{title1_list[i]} - {title2_list[i]} \n{desc_list[i]} \nLink: {link_list[i]}\n')
-    except:
-        continue
+        title1 = soup.find_all('span', {'id': 'part1'})
+        title2 = soup.find_all('span', {'id': 'part2'})
+        desc = soup.find_all('p', {'id': 'banner_description'})
+        link = soup.find_all('a', {'class': 'liferay-home-banner-action'})
+
+        title1_list = [i1.text for i1 in title1]
+        title2_list = [i2.text for i2 in title2]
+        desc_list = [i3.text.strip() for i3 in desc]
+        link_list = [i4['href'] for i4 in link]
+
+        print('Promoções Smiles \n')
+        for i, promo in enumerate(title1_list):
+            try:
+                print(
+                    f'{title1_list[i]} - {title2_list[i]} \n{desc_list[i]} \nLink: {link_list[i]}\n')
+            except:
+                continue
+
+    def latam_pass(self):
+        latam_pass_url = 'https://latampass.latam.com/pt_br/'
+
+        page = requests.get(latam_pass_url, verify=False)
+        resposta = page.text
+        soup = BeautifulSoup(resposta, 'html.parser')
+
+        title1 = soup.find_all('a', {'class': 'sc'})
+
+        print(title1)
+
+        title1_list = [i1.text for i1 in title1]
+
+        print(title1_list)
+
+        '''
+        title2 = soup.find_all('span', {'id': 'part2'})
+        desc = soup.find_all('p', {'id': 'banner_description'})
+        link = soup.find_all('a', {'class': 'liferay-home-banner-action'})
+
+        title1_list = [i1.text for i1 in title1]
+        title2_list = [i2.text for i2 in title2]
+        desc_list = [i3.text.strip() for i3 in desc]
+        link_list = [i4['href'] for i4 in link]
+
+        print('Promoções Smiles \n')
+        for i, promo in enumerate(title1_list):
+            # print(title1_list)
+            try:
+                print(
+                    f'{title1_list[i]} - {title2_list[i]} \n{desc_list[i]} \nLink: {link_list[i]}\n')
+            except:
+                continue           
+        '''
+
+
+self = Milhas()
